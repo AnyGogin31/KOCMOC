d2dd463f1394b1bfee34e2f149f63f3b3fa4d4eb
AnyGogin31 <148564043+AnyGogin31@users.noreply.github.com>
2024-10-04 17:58:09 +0300

    Adapt point generation to dynamic screen size in Canvas
    - Integrated dynamic screen width and height using `LocalConfiguration` and `LocalDensity`.
    - Modified `generateRandomPoints` to use actual screen dimensions for random point placement.
    - Added an extension function to convert `Dp` to pixels.

--- app/src/main/java/neilt/mobile/kocmoc/MainActivity.kt
+++ app/src/main/java/neilt/mobile/kocmoc/MainActivity.kt
@@ -16,6 +16,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.delay
 import neilt.mobile.kocmoc.ui.theme.KocmocTheme
 import kotlin.random.Random
@@ -34,11 +38,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun KocmocCanvas() {
-    var points by remember { mutableStateOf(generateRandomPoints()) }
+    var points by remember { mutableStateOf(generateRandomPoints(0f, 0f)) }
+
+    val configuration = LocalConfiguration.current
+    val maxWidth = configuration.screenWidthDp.dp.toPx()
+    val maxHeight = configuration.screenHeightDp.dp.toPx()
 
     LaunchedEffect(Unit) {
         while (true) {
-            points += generateRandomPoints()
+            points += generateRandomPoints(maxWidth, maxHeight)
             if (points.size > 1000) {
                 points = points.takeLast(1000)
             }
@@ -59,12 +67,12 @@ fun KocmocCanvas() {
     }
 }
 
-fun generateRandomPoints(): List<Point> {
+fun generateRandomPoints(maxWidth: Float, maxHeight: Float): List<Point> {
     val random = Random(System.currentTimeMillis())
     return List(5) {
         Point(
-            x = random.nextFloat() * 1080f,
-            y = random.nextFloat() * 1920f,
+            x = random.nextFloat() * maxWidth,
+            y = random.nextFloat() * maxHeight,
             radius = random.nextFloat() * random.nextInt(20),
             color = Color(
                 random.nextInt(256),
@@ -81,3 +89,8 @@ data class Point(
     val radius: Float,
     val color: Color,
 )
+
+@Composable
+fun Dp.toPx(): Float {
+    return with(LocalDensity.current) { this@toPx.toPx() }
+}
